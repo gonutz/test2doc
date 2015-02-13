10db7d912a6563c78d728e2fa4c1fad3a6a286b3
lars.scheme <lars.scheme@localhost>
2015-02-13 13:07:48 +0100

    Refactored big method into smaller ones.

--- t2d/doc_creator.go
+++ t2d/doc_creator.go
@@ -13,31 +13,48 @@ type DocCreator struct {
 	formatter DocFormatter
 }
 
-func NewDocCreator(fc FileCollector, ff FileFilter, te TestExtractor, c NameChopper, df DocFormatter) DocCreator {
+func NewDocCreator(
+	fc FileCollector,
+	ff FileFilter,
+	te TestExtractor,
+	c NameChopper,
+	df DocFormatter) DocCreator {
 	return DocCreator{fc, ff, te, c, df}
 }
 
 func (doc DocCreator) CreateDocFromFolder(path string) (string, error) {
 	err := doc.collector.Collect(path)
 	if err != nil {
-		withGoPath := filepath.Join(os.Getenv("GOPATH"), "src", path)
-		withGoPath = filepath.ToSlash(withGoPath)
-		err = doc.collector.Collect(withGoPath)
+		err = doc.tryCollectingRelativeToGOPATH(path)
 		if err != nil {
 			return "", err
 		}
 	}
 	for _, path := range doc.collector.Paths() {
-		if doc.filter.IsValid(path) {
-			testNames, err := doc.extractor.ExtractTestsFromFile(path)
-			if err == nil {
-				sentences := make([][]string, 0, 20)
-				for _, name := range testNames {
-					sentences = append(sentences, doc.chopper.Chop(name))
-				}
-				doc.formatter.Append(path, sentences)
-			}
-		}
+		doc.extractTestsIfTestFile(path)
 	}
 	return doc.formatter.Format(), nil
+}
+
+func (doc DocCreator) tryCollectingRelativeToGOPATH(path string) error {
+	withGoPath := filepath.Join(os.Getenv("GOPATH"), "src", path)
+	withGoPath = filepath.ToSlash(withGoPath)
+	return doc.collector.Collect(withGoPath)
+}
+
+func (doc DocCreator) extractTestsIfTestFile(path string) {
+	if doc.filter.IsValid(path) {
+		testNames, err := doc.extractor.ExtractTestsFromFile(path)
+		if err == nil {
+			doc.appendChoppedTestNames(testNames, path)
+		}
+	}
+}
+
+func (doc DocCreator) appendChoppedTestNames(testNames []string, path string) {
+	sentences := make([][]string, 0, 20)
+	for _, name := range testNames {
+		sentences = append(sentences, doc.chopper.Chop(name))
+	}
+	doc.formatter.Append(path, sentences)
 }
